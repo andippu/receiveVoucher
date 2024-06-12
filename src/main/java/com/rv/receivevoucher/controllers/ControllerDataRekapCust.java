dfa26a97cbfff1c3b687394f2470899370ab3876
andirhl <andi@ppu.co.id>
2024-06-12 16:24:34 +0700

    add data rekap cust done - by am

--- src/main/java/com/rv/receivevoucher/controllers/ControllerDataRekapCust.java
+++ src/main/java/com/rv/receivevoucher/controllers/ControllerDataRekapCust.java
@@ -17,17 +17,17 @@ public class ControllerDataRekapCust {
 	@Autowired
 	ServiceDataRekapCust servDrcs;
 	
-	 @GetMapping("/fakmas/datarkpcust")
+	@GetMapping("/fakmas/datarkpcust")
 	public String getDataRekapCust(String P_DISTCODE, String P_TYPE, String P_DATE, String P_MATERAI,
             String P_MATERAI2, String P_MATERAI3) {
 		 System.out.println("aaaaa :"+P_DATE);
 		return servDrcs.getDataRekapCust(P_DISTCODE, P_TYPE, P_DATE, P_MATERAI, P_MATERAI2, P_MATERAI3);
 	}
 	 
-	 @GetMapping("/fakmas/datarkpcustlist")
+	@GetMapping("/fakmas/datarkpcustlist")
 	 public List<DataRekapCust> getDtRkpCust (){
 			return servDrcs.getDtRkpCustList();
-		}
+	}
 	 
 	
 }
